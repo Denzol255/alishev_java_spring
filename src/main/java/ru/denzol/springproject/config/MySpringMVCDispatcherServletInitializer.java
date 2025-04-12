b28da3f63d22cf611857e6887f352c452b4adcc5
Denzol255 <den-zoloto@mail.ru>
2025-04-12 19:27:37 +0300

    XML Config

---
+++ src/main/java/ru/denzol/springproject/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package ru.denzol.springproject.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Цирк</h2>
 </body>
 </html>
